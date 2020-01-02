8a3d935d6625026931437f008f24fce58952e1d7
Dakota Nichols <dakotanichols0996@gmail.com>
2020-01-02 16:11:08 -0600

    Created the app and made a very simple calculator using Java and the items covered in chapter one.

---
+++ Chapter 1/Chapter1Project/src/app/FirstApp.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.util.Scanner;
+
+// Simple calculator
+
+class FirstApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Would you like to Add, Subtract, Multiply, or Divide?: ");
+        String choice = input.nextLine();
+        if (choice.equals("add")) {
+            System.out.println("Give me a number: ");
+            Integer number = input.nextInt();
+            System.out.println("Give me another number: ");
+            Integer numberTwo = input.nextInt();
+            Integer sum = number + numberTwo;
+            System.out.println("The total is: " + sum);
+        }
+        if (choice.equals("subtract")) {
+            System.out.println("Give me a number: ");
+            Integer number = input.nextInt();
+            System.out.println("Give me another number: ");
+            Integer numberTwo = input.nextInt();
+            Integer sub = number - numberTwo;
+            System.out.println("The total is: " + sub);
+        }
+        if (choice.equals("multiply")) {
+            System.out.println("Give me a number: ");
+            Integer number = input.nextInt();
+            System.out.println("Give me another number: ");
+            Integer numberTwo = input.nextInt();
+            Integer mult = number * numberTwo;
+            System.out.println("The total is: " + mult);
+        }
+
+        input.close();
+    }
+}
